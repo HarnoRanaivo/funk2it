1bc96500ec324e7e8ac50c5364ae52026d25ab4b
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2013-06-14 16:14:43 +0200

    tests appels imbriqués

--- bacasable.ml
+++ bacasable.ml
@@ -1,8 +1,9 @@
 #use "schemas.ml";;
-(*
-let factorielleRec = recursiveTerminale 1 ((=) 0) ((+) (-1)) ( * );;
-let factorielleIt = iterative 1 ((=) 0) ((+) (-1)) ( * );;
 
+(*let factorielleRec = recursiveTerminale 1 ((=) 0) ((+) (-1)) ( * );;*)
+(* let factorielleIt = iterative 1 ((=) 0) ((+) (-1)) ( * ) ;; *)
+
+(*
 let teteListe liste = match liste with
     [] -> []
     | t :: q ->  t
@@ -25,6 +26,7 @@ let listeEstVide liste = match liste with
     | t :: q -> false
 ;;
 
+(*
 type 'a arbre =
     Feuille of 'a
     | Noeud of 'a arbre * 'a arbre
@@ -71,7 +73,7 @@ let hArbre a =
     and droite (Noeud(g, d)) = d
     and plusplus x = x + 1
     and max a b = if a > b then a else b
-    in (recursiveTerminale2 1 estFeuille gauche droite max plusplus) a
+    in (recursiveTerminale2 1 estFeuille gauche droite max plusplus plusplus) a
 ;;
 
 let rec hauteurArbre2aux_2 a n m =
@@ -88,16 +90,6 @@ let arbre2 = (Noeud(Noeud(Feuille(1), Feuille(2)), Noeud(Noeud(Feuille(1), Feuil
 let arbre3 = (Noeud(Noeud(Feuille(1), Feuille(2)), Noeud(Noeud(Noeud(Feuille(1), Feuille(2)), Feuille(3)), Feuille(1))));;
 let arbre4 = (Noeud(Noeud(Noeud(Noeud(Feuille(1), Feuille(2)), Feuille(3)), Feuille(1)), Noeud(Feuille(1), Feuille(2))));;
 
-(*
-hauteurArbre2 arbre1;;
-hauteurArbre2 arbre2;;
-hauteurArbre2 arbre3;;
-
-hauteurArbre2_2 arbre1;;
-hauteurArbre2_2 arbre2;;
-hauteurArbre2_2 arbre3;;
-hauteurArbre2_2 arbre4;;
- *)
 
 (*
 hauteurArbre2 arbre4;;
@@ -112,22 +104,99 @@ hauteurArbre2_2 arbre5;;
 
 hauteurArbreRec arbre5;;
 hArbre arbre5;;
+*)
 
+(*
 let rec fibo1 n =
     if n = 0  || n = 1 then 1
     else (+) (fibo1 (n-1)) (fibo1 (n-2))
 ;;
 
 let fibo2 n =
-    let zeroUn x = if x = 0 || x = 1 then true else false
+    let zeroUn x = x = 0 || x = 1
     and moins1 x = x - 1
     and moins2 x = x - 2
     and plus x y = x + y
     and identite x = x
-    in recursiveTerminale2 1 zeroUn moins1 moins2 plus identite n
+    in recursiveTerminale2 1 zeroUn moins1 moins2 plus identite identite n
 ;;
 
-#trace recursiveTerminale;;
-#trace recursiveTerminale2;;
-fibo1 1;;
-fibo2 1;;
+let fibo3 n =
+    let zeroUn x = x = 0 || x = 1
+    and moins1 x = x - 1
+    and moins2 x = x - 2
+    and plus x y = x + y
+    and identite x = x
+    in iterative2 1 zeroUn moins1 moins2 plus identite identite n
+;;
+
+let fibo4 n =
+    let rec aux n a b =
+        if n = 0 || n = 1 then b
+        else aux (n-1) b (a+b)
+    in aux n 1 1
+;;
+
+fibo1 9;;
+fibo2 9;;
+fibo3 9;;
+fibo4 9;;
+ *)
+
+let rec mc91 n =
+    if n > 100 then n - 10
+    else mc91 (mc91 (n + 11))
+;;
+
+let mc91Iter n =
+    let c = (ref 1)
+    and resultat = (ref n) in
+    while !c <> 0 do
+        if !resultat > 100 then
+        (
+            resultat := !resultat - 10;
+            c := !c - 1;
+        )
+        else
+        (
+            resultat := !resultat + 11;
+            c := !c + 1;
+        )
+    done;
+    !resultat
+;;
+
+let mc91RecTer n =
+    let rec aux n m =
+        if m = 0 then n
+        else if n > 100 then aux (n-10) (m-1)
+        else aux (n+11) (m+1)
+    in aux n 1
+;;
+
+mc91 1;;
+mc91Iter 1;;
+mc91RecTer 1;;
+
+(*
+let rec f n =
+    if n >= 20 then n / 2
+    else 3 + n + (f (n + 5))
+;;
+
+let rec aux n a =
+    if n >= 20 then a + n/2
+    else aux (n+5) (n+a+3)
+;;
+
+let f2 n = aux n 0;;
+let rec aux2 n a =
+    if n = 0 then a
+    else aux2 (n-1) (a*n)
+;;
+
+let faa n = aux2 n 1;;
+f 5;;
+f2 5;;
+faa 4;;
+ *)
